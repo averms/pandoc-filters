562f2cd11ea5c47e4d16c70e4380538740bf6713
Aman Verma <amanraoverma@gmail.com>
2020-10-31 18:04:10 -0400

    code-includes.lua: Including and attribute improvements.
    Don't strip all whitespace on including a file, only the last newline.
    Also delete the `inc` attribute after it has been detected, it doesn't
    need to be in the output.

--- dist/code-includes.lua
+++ dist/code-includes.lua
@@ -4,22 +4,24 @@
 -- helper funcs
 local function readPopulatedLines(fpth)
   io.input(fpth)
-  local contents = io.read("a")
+  local contents = io.read('a')
   -- Unix text editors save files with a newline at the end.
   -- This is not useful to us so we strip it.
-  contents = contents:gsub("%s+$", "")
+  contents = contents:gsub("\n$", '')
   return contents
 end
 
 return {
   {
     CodeBlock = function(cb)
-      if cb.attributes["inc"] then
-        -- strip all whitespace around the text
-        filename = cb.text:match("^%s*(.-)%s*$")
+      if cb.attributes['inc'] then
+        -- Strip all whitespace around the text.
+        filename = cb.text:match('^%s*(.-)%s*$')
+        -- Remove the inc attribute so it doesn't show up in output.
+        cb.attributes['inc'] = nil
 
-        if filename == "" then
-          io.stderr:write("There was no filename inside the code block.")
+        if filename == '' then
+          io.stderr:write('There was no filename inside the code block.')
           return cb
         end
 
